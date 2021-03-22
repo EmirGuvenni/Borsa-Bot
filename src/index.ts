ca13f4c52af3b2c38e9f71126ff161840d63664c
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2021-03-22 14:56:00 +0300

    New featues and bug fixed
    New feature:
    - Now it supports BTC, BAT and ETH to USD
    
    BUG:
    - Fixed a bug where money exchange rates would be cut if the rate is 10 or higher.

--- src/index.ts
+++ src/index.ts
@@ -22,16 +22,44 @@ client.on('ready', async () => {
 client.on('message', async (message) => {
 	if (message.content == '!borsa') {
 		let rates = await getRates();
+		let crypto = await getCrypto();
 
 		await message.channel.send([
 			`Dolar: ${rates.usd}₺`,
-			`Euro: ${rates.eur}₺`
+			`Euro: ${rates.eur}₺`,
+			`BTC: ${crypto.btc}$`,
+			`BAT: ${crypto.bat}$`,
+			`ETH: ${crypto.eth}$`
 		]);
 	}
 })
 
 client.login(config.token);
+async function getCrypto() {
+	const btcReq = await axios.get('https://data.messari.io/api/v1/assets/btc/metrics/market-data');
+	const batReq = await axios.get('https://data.messari.io/api/v1/assets/bat/metrics/market-data');
+	const ethReq = await axios.get('https://data.messari.io/api/v1/assets/eth/metrics/market-data');
 
+	const btc: string = btcReq.data.data.market_data.price_usd;
+	const bat: string = batReq.data.data.market_data.price_usd;
+	const eth: string = ethReq.data.data.market_data.price_usd;
+
+	const btcDollar = btc.split('.')[0];
+	const batDollar = bat.split('.')[0];
+	const ethDollar = eth.split('.')[0];
+
+
+	const btcCents = btc.split('.')[1].substring(0, 2);
+	const batCents = bat.split('.')[1].substring(0, 2);
+	const ethCents = eth.split('.')[1].substring(0, 2);
+
+	return {
+		btc: `${btcDollar}.${btcCents}`,
+		bat: `${batDollar}.${batCents}`,
+		eth: `${ethDollar}.${ethCents}`
+	}
+
+}
 async function getRates() {
 	let usdReq = await axios.get('https://www.freeforexapi.com/api/live?pairs=USDTRY');
 	let eurusdReq = await axios.get('https://www.freeforexapi.com/api/live?pairs=EURUSD');
@@ -39,8 +67,14 @@ async function getRates() {
 	let _usd: number = usdReq.data.rates.USDTRY.rate;
 	let _eur: number = eurusdReq.data.rates.EURUSD.rate * _usd;
 
-	let usd: string = String(_usd).slice(0, 4);
-	let eur: string = String(_eur).slice(0, 4);
+	const usdP: string = String(_usd).split('.')[0];
+	const eurP: string = String(_eur).split('.')[0];
+
+	const usdS: string = String(_usd).split('.')[1].substring(0, 2);
+	const eurS: string = String(_eur).split('.')[1].substring(0, 2);
+
+	let usd: string = `${usdP}.${usdS}`;
+	let eur: string = `${eurP}.${eurS}`;
 
 	return { usd, eur };
 }
